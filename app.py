06f5a4b9424e939f07fb2b14660c4b8f059c2c07
Adam zouhairi <97172783+AdamZouhairii@users.noreply.github.com>
2024-03-31 16:50:20 +0200

    up

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve,auc
+from sklearn.metrics import roc_curve, roc_auc_score
 import xgboost as xgb
 import warnings
 import plotly.figure_factory as ff
@@ -20,25 +20,47 @@ import plotly.graph_objs as go
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
+st.set_page_config(
+    page_title="stellar_classification_ML",
+    page_icon="🌌",
+)
 
 data = r'C:\Users\Adame\Documents\GitHub\Stellar-Classification-ML\star_classification.csv'
 df = pd.read_csv(data)
 head = df.head()
-info = df.info()
 describe = df.describe()
 missing = df.isnull().sum()
 class_counts = df["class"].value_counts()
 
-# Creating a new figure and axis
-fig, ax = plt.subplots()
 
-# Plotting a bar chart
-ax.bar(class_counts.index, class_counts.values, color='red')
+# Plotting the value counts of classes --------------------------------#
+fig, ax = plt.subplots(facecolor='black')                              #
+ax.bar(class_counts.index, class_counts.values, color='red')           #
+ax.grid(False)                                                         #
+ax.set_xlabel('Class', color='white')                                  #
+ax.set_ylabel('Count', color='white')                                  #
+ax.set_title('Value Counts of Classes', color='white')                 #
+ax.set_facecolor('black')                                              #
+ax.tick_params(axis='x', colors='white', rotation=45)                  #
+ax.tick_params(axis='y', colors='white')                               #
+ax.set_xticks(range(len(class_counts.index)))                          #
+ax.set_xticklabels(class_counts.index, rotation=45, ha='right')        #
+# ---------------------------------------------------------------------#
 
-# Setting labels and title
-ax.set_xlabel('Class')
-ax.set_ylabel('Count')
-ax.set_title('Value Counts of Classes')
+# Percentage distribution of class types ------------------------------------------------------#
+colors = ['#48678b', '#748db4', '#b3c1d2']
+pull = [0.015, 0.025, 0.025] 
+pdct = go.Figure(data=[go.Pie(labels=class_counts.index,
+                             values=class_counts.values,
+                             pull=pull,
+                             marker_colors=colors,
+                             textinfo='percent+label',
+                             textfont=dict(family='Times New Roman', size=12),
+                             )])
+pdct.update_layout(title_text='Percentage Distribution of Class Types',
+                  title_font=dict(family='Times New Roman', size=16),
+                  )
+#-----------------------------------------------------------------------------------------------#
 
 df["class"] = [0 if i == 'GALAXY' else 1 if i == 'STAR' else 2 for i in df["class"]]
 
@@ -54,13 +76,25 @@ filtre = outlier_score["score"] < threshold
 outlier_index = outlier_score[filtre].index.tolist()
 df.drop(outlier_index, inplace = True)
 
-
+# Correlation Matrix --------------------------------------------------------------------------#
+df = df.drop(['rerun_ID'], axis=1)
 corr = df.corr()
 f, ax = plt.subplots(figsize=(13, 9))
-sns.heatmap(corr, cmap="YlOrBr" , annot=True, linewidths=0.5, fmt= '.2f',ax=ax)
-ax.set_title('Correlation Matrix')
+ax.set_facecolor('black')
+sns_heatmap = sns.heatmap(corr, cmap="YlOrBr", annot=True, linewidths=0.5, fmt='.2f', ax=ax)
+f.set_facecolor('black')
+ax.set_xlabel(ax.get_xlabel(), color='white')
+ax.set_ylabel(ax.get_ylabel(), color='white')
+ax.set_xticklabels(ax.get_xticklabels(), color='white')
+ax.set_yticklabels(ax.get_yticklabels(), color='white')
+cbar = ax.collections[0].colorbar
+cbar.ax.yaxis.set_tick_params(color='white')
+plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), color='white')
+ax.set_title('Correlation Matrix', color='white')       
+#-----------------------------------------------------------------------------------------------#
 
-df = df.drop(['obj_ID','alpha','delta','run_ID','rerun_ID','cam_col','field_ID','fiber_ID'], axis = 1)
+
+df = df.drop(['obj_ID','alpha','delta','run_ID','cam_col','field_ID','fiber_ID'], axis = 1)
 x = df.drop(['class'], axis = 1)
 y = df.loc[:,'class'].values
 scaler = StandardScaler()
@@ -77,28 +111,22 @@ clf_score = np.mean(score)
 
 classes = ["GALAXY", "STAR", "QSO"]
 
+# Compute confusion matrix --------------------------------------------------------------------#
 # Compute confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 # Create annotated heatmap using Plotly
 r_forest_cm = ff.create_annotated_heatmap(z=cm, x=classes, y=classes, colorscale='YlOrBr', showscale=True)
 # Update layout
 r_forest_cm.update_layout(title='Confusion Matrix', xaxis=dict(title='Predicted label'), yaxis=dict(title='True label', autorange="reversed"))
+#-----------------------------------------------------------------------------------------------#
 
-
-
-# Generate classification report
+# Generate classification report----------------------------------------------------------------#
 r_forest_cfr = classification_report(y_test, y_pred, target_names=classes, output_dict=True)
-
-# Extract values from classification report
 report_data = []
 for label, metrics in r_forest_cfr.items():
     if label in classes:
         report_data.append([label, metrics['precision'], metrics['recall'], metrics['f1-score'], metrics['support']])
-
-# Convert list to DataFrame
 report_df = pd.DataFrame(report_data, columns=['Class', 'Precision', 'Recall', 'F1-Score', 'Support'])
-
-# Create table trace with black background
 table_trace = go.Table(
     header=dict(values=report_df.columns.tolist(), fill=dict(color='black'),  # Change header background color to blue
                 font=dict(color='white'),  # Change header font color to white
@@ -107,17 +135,12 @@ table_trace = go.Table(
                fill=dict(color='black'),  # Change cell background color to black
                font=dict(color='white'),  # Change cell font color to white
                align='center'))
-
-# Create figure
 fig_r_forest_cfr = go.Figure()
-
-# Add table trace to figure
 fig_r_forest_cfr.add_trace(table_trace)
-
-# Update layout
-fig_r_forest_cfr.update_layout(title='Classification Report', title_x=0.5)
-
-
+fig_r_forest_cfr.update_layout(title='Classification Report')
+#-----------------------------------------------------------------------------------------------#
+roc_forest = 'roc_forest.png'
+cpe_forest = 'cpe_forest.png'
 
 
 svm_clf = svm.SVC(kernel='rbf', C=1, random_state=0)
@@ -127,13 +150,16 @@ score = svm_clf.score(X_test, y_test)
 svm_score_ = np.mean(score)
 
 
-# Compute confusion matrix
+# Compute confusion matrix---------------------------------------------------------------------#
 cm = confusion_matrix(y_test, predicted)
 # Create annotated heatmap using Plotly
 svm_cm = ff.create_annotated_heatmap(z=cm, x=classes, y=classes, colorscale='YlOrBr', showscale=True)
 # Update layout
 svm_cm.update_layout(title='Confusion Matrix', xaxis=dict(title='Predicted label'), yaxis=dict(title='True label', autorange="reversed"))
+#-----------------------------------------------------------------------------------------------#
 
+roc_svm = 'roc_svm.png'
+cpe_svm = 'cpe_svm.png'
 
 
 # Use XGBoost classifier with default parameters
@@ -144,17 +170,21 @@ y_pred_xgb = xgb_clf.predict(X_test)
 xgb_score = xgb_clf.score(X_test, y_test)
 xgb_score = np.mean(xgb_score)
 
-# Compute confusion matrix
+# Compute confusion matrix---------------------------------------------------------------------#
 cm = confusion_matrix(y_test, y_pred_xgb)
 # Create annotated heatmap using Plotly
 xgb_cm = ff.create_annotated_heatmap(z=cm, x=classes, y=classes, colorscale='YlOrBr', showscale=True)
 # Update layout
 xgb_cm.update_layout(title='Confusion Matrix', xaxis=dict(title='Predicted label'), yaxis=dict(title='True label', autorange="reversed"))
+#-----------------------------------------------------------------------------------------------#
 
-st.set_page_config(
-    page_title="stellar_classification_ML",
-    page_icon="🌌",
-)
+roc_xgb = 'roc_xgb.png'
+cpe_xgb = 'cpe_xgb.png'
+
+"""
+   Stellar Classification ML App
+
+"""
 
 st.title('Star Classification')
 st.write('This app uses a dataset of stars to classify them into three classes: GALAXY, STAR, QSO')
@@ -164,11 +194,37 @@ st.dataframe(head)
 st.write('The dataset has the following class for the classification prediction:')
 st.write('GALAXY, STAR, QSO')
 st.pyplot(fig)
+st.write('The dataset has the following percentage distribution of class types')
+st.plotly_chart(pdct)
 st.write('The dataset has the following correlation matrix')
 st.write('The correlation matrix shows the correlation between the different features in the dataset')
 st.pyplot(f)
 st.write('The dataset has the following information')
-st.dataframe(info)
+st.write("""
+         RangeIndex: 100000 entries, 0 to 99999
+Data columns (total 18 columns):
+ #   Column       Non-Null Count   Dtype
+---  ------       --------------   -----
+ 0   obj_ID       100000 non-null  float64
+ 1   alpha        100000 non-null  float64
+ 2   delta        100000 non-null  float64
+ 3   u            100000 non-null  float64
+ 4   g            100000 non-null  float64
+ 5   r            100000 non-null  float64
+ 6   i            100000 non-null  float64
+ 7   z            100000 non-null  float64
+ 8   run_ID       100000 non-null  int64
+ 9   rerun_ID     100000 non-null  int64
+ 10  cam_col      100000 non-null  int64
+ 11  field_ID     100000 non-null  int64
+ 12  spec_obj_ID  100000 non-null  float64
+ 13  class        100000 non-null  object
+ 14  redshift     100000 non-null  float64
+ 15  plate        100000 non-null  int64
+ 16  MJD          100000 non-null  int64
+ 17  fiber_ID     100000 non-null  int64
+dtypes: float64(10), int64(7), object(1)
+memory usage: 13.7+ MB""")
 st.write('The dataset has no missing values')
 st.dataframe(missing)
 st.write('The dataset has the following description')
@@ -184,8 +240,20 @@ st.write('Confusion Matrix', 'Classification Report', 'ROC AUC', 'Class Predicti
 st.write('The accuracy of the classifiers is as follows:')
 st.write('Random Forest Classifier', clf_score)
 st.plotly_chart(r_forest_cm)
+st.plotly_chart(fig_r_forest_cfr)
+st.write('RoC AUC for Random Forest Classifier')
+st.image(roc_forest)
+st.write('Class Prediction Error for Random Forest Classifier')
+st.image(cpe_forest)
 st.write('Support Vector Machine Classifier (SVM)', svm_score_)
 st.plotly_chart(svm_cm)
+st.write('RoC AUC for Support Vector Machine Classifier (SVM)')
+st.image(roc_svm)
+st.write('Class Prediction Error for Support Vector Machine Classifier (SVM)')
+st.image(cpe_svm)
 st.write('XGBoost Classifier (XGB)', xgb_score)
 st.plotly_chart(xgb_cm)
-st.plotly_chart(fig_r_forest_cfr)
+st.write('RoC AUC for XGBoost Classifier (XGB)')
+st.image(roc_xgb)
+st.write('Class Prediction Error for XGBoost Classifier (XGB)')
+st.image(cpe_xgb)
